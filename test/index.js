9745f46a48b9c95bb2572e4c1390d301edbe5d8c
ccwq <scwqd@qq.com>
2020-11-03 21:51:11 +0800

    backup

--- test/index.js
+++ test/index.js
@@ -3,9 +3,17 @@ const har = require("../");
 const fs = require("fs");
 const path = require("path");
 
-har.fromText(fs.readFileSync(path.join(__dirname, "demo.HAR"), 'utf-8'), path.join(__dirname, "dist"), function(err) {
+
+const harPath = path.join(__dirname, "demo.HAR");
+const distPath = path.join(__dirname, "dist");
+
+har.fromText(
+	fs.readFileSync(harPath, 'utf-8'),
+	distPath,
+	function(err) {
     console.log(err);
-    har.formFile(path.join(__dirname, "demo.HAR"), path.join(__dirname, "dist1"), function(err) {
-        console.log(err);
-    });
-});
+		har.formFile(path.join(__dirname, "demo.HAR"), path.join(__dirname, "dist1"), function(err) {
+			console.log(err);
+		});
+	}
+);
